260d30c9f00970c9bdd31362a40d364033c73a28
kwYuk <kwyuk@newbizstart.com>
2022-08-24 20:45:13 +0900

    update dashboard page

--- components/layouts/mainFooter/index.tsx
+++ components/layouts/mainFooter/index.tsx
@@ -1,13 +1,36 @@
 import NextLink from "next/link";
+import Script from "next/script";
 
 export default function MainFooter() {
   return (
-    <footer className="sticky-footer bg-white">
-      <div className="container my-auto">
-        <div className="copyright text-center my-auto">
-          <span>Copyright &copy; Your Website 2021</span>
+    <>
+      <footer className="sticky-footer bg-white">
+        <div className="container my-auto">
+          <div className="copyright text-center my-auto">
+            <span>Copyright &copy; Your Website 2021</span>
+          </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+      {/* Bootstrap core JavaScript */}
+      <Script
+        src="/assets/vendor/jquery/jquery.min.js"
+        strategy="beforeInteractive"
+      ></Script>
+      <Script
+        src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"
+        strategy="beforeInteractive"
+      ></Script>
+      {/* Core plugin JavaScript */}
+      <Script
+        src="/assets/vendor/jquery-easing/jquery.easing.min.js"
+        strategy="beforeInteractive"
+      ></Script>
+
+      {/* Custom scripts for all pages */}
+      <Script
+        src="/assets/js/sb-admin-2.min.js"
+        strategy="beforeInteractive"
+      ></Script>
+    </>
   );
 }
---
+++ pages/components/buttons/index.tsx
@@ -0,0 +1,15 @@
+import { ReactElement } from "react";
+import Layout from "../../../components/layouts";
+// import IndexPage from "../../components/dashboard/indexPage";
+
+index.getLayout = function getLayout(page: ReactElement) {
+  return <Layout variant="default">{page}</Layout>;
+};
+
+export default function index() {
+  return (
+    <>
+      <p>Components Button</p>
+    </>
+  );
+}
